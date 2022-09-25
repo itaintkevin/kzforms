6d77c3cf2dcb3c8e6429f2a00121a843a54ee405
itaintkevin <kpjtofficial@gmail.com>
2022-09-25 16:23:24 +0530

    Added Submit Button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import FormInput from './FormInput';
 
 const Form = () => {
   return (
-    <div className='flex justify-center text-white my-24 scroll-smooth'>
+    <div className='text-white mt-24 pb-24 pt-5 scroll-smooth'>
         <form className='w-screen'>
             <FormInput condition="" placeholder="First Name"/>
             <FormInput condition="" placeholder="Last Name"/>
@@ -12,6 +12,9 @@ const Form = () => {
             <FormInput condition="" placeholder="Github Profile"/>
             <FormInput condition="" placeholder="Course"/>
             <FormInput condition="" placeholder="Year"/>
+            <div className='from-[#FB5A0D] to-[#EB144C] text-white text-center text-xl font-semibold bg-gradient-to-r drop-shadow-2xl rounded-md mx-5 px-5 py-2'>
+              <button type="submit">Sumbit</button>
+            </div>
         </form>
     </div>
   )
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FormInput = (props) => {
   return (
     <div className='flex items-center justify-between my-3'>
-        <input className='bg-[#202020] w-full mx-5 px-5 py-2' placeholder={props.placeholder}/>
+        <input className='bg-[#202020] w-full rounded-md mx-5 px-5 py-2' placeholder={props.placeholder}/>
         <label>{props.condition}</label>
     </div>
   )
